761c95509cb79a098245ac0440cb4e693f780e52
Stanislav Ermilov <shindji19922904@gmail.com>
2023-09-20 12:14:04 +0700

    update styles for empty_basket and single_pizza

--- src/components/EmptyBasket.tsx
+++ src/components/EmptyBasket.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import {Link} from "react-router-dom";
 import empty_basket from '../assets/img/empty-cart.png'
+import styles from '../styles/components/empty_basket.module.scss'
 
 export const EmptyBasket: React.FC = () => {
     return (
-        <div className="cart cart--empty">
+        <div className={styles.empty_basket}>
             <h2>Корзина пустая</h2>
             <p>
                 Вероятней всего, вы не заказывали ещё пиццу.<br/>
                 Для того, чтобы заказать пиццу, перейди на главную страницу.
             </p>
             <img src={empty_basket} alt="Empty cart"/>
-            <Link to="/" className="button button--black">
+            <Link to="/" className={styles.button_black}>
                 <span>Вернуться назад</span>
             </Link>
         </div>
